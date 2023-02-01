dda2ce47d165b1b4824c2f6f779f70e76905d7bd
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 00:19:47 +0100

    chore: adding profile preview function

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Container, Row, Col } from "react-bootstrap";
@@ -8,6 +8,36 @@ import botImg from "../assets/images/bot.jpeg";
 import "./signup.css";
 
 export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  //image upload status
+  const [image, setImage] = useState(null);
+  const [uploadingImage, setUploadingImage] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const validateImg = (e) => {
+    const file = e.target.files[0];
+    if (file.size >= 1048576) {
+      alert("Max file size is 1mb");
+    } else {
+      setImage(file);
+      setImagePreview(URL.createObjectURL(file));
+    }
+  };
+
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append("file", image);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!image) return alert("please upload your profile image");
+    const url = await uploadImage();
+  };
+
   return (
     <Container>
       <Row>
@@ -15,10 +45,14 @@ export const Signup = () => {
           md={7}
           className="d-flex align-items-center justify-content-center flex-direction-column"
         >
-          <Form style={{ width: "80%", maxWidth: 500 }}>
+          <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleSubmit}>
             <h1 className="text-center">Create account</h1>
             <div className="signup-profile-pic__container">
-              <img src={botImg} alt="" className="signup-profile-pic" />
+              <img
+                src={imagePreview || botImg}
+                alt=""
+                className="signup-profile-pic"
+              />
               <label htmlFor="image-upload" className="image-upload-label">
                 <i className="fas fa-plus-circle add-picture-icon"></i>
               </label>
@@ -28,16 +62,27 @@ export const Signup = () => {
                 hidden
                 accept="image/png,
               image/jpeg"
+                onChange={validateImg}
               />
             </div>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Your name" />
+              <Form.Control
+                type="text"
+                placeholder="Your name"
+                value={name}
+                onChange={({ target }) => setName(target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -45,7 +90,12 @@ export const Signup = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
             </Form.Group>
 
             <Button variant="primary" type="submit">
