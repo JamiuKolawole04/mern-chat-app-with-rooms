dff985f4b2a1a742c47728329acab15d80c6840e
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-01 22:55:03 +0100

    chore: adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Signup, Login, Home, Chat } from "./pages";
 import "./App.css";
+import { Navigation } from "./components";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { Home } from "./home";
+export { Chat } from "./chat";
+export { Login } from "./login";
+export { Signup } from "./signup";
