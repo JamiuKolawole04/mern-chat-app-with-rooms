7b420ce1eb92f33495363b297992d57316cf81ba
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 23:46:11 +0100

    feat: appApi service created

--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -1,5 +1,3 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-
 /* React-specific entry point that automatically generates
    hooks corresponding to the defined endpoints */
 import { createApi } from "@reduxjs/toolkit/query/react";
@@ -13,10 +11,34 @@ const appApi = createApi({
     // registering or creating users
     signUpUser: builder.mutation({
       query: (user) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: user,
+      }),
+    }),
+
+    loginUser: builder.mutation({
+      query: (user) => ({
         url: "/auth/login",
         method: "POST",
         body: user,
       }),
     }),
+
+    logoutUser: builder.mutation({
+      query: (payload) => ({
+        url: "/auth/logout",
+        method: "DELETE",
+        body: payload,
+      }),
+    }),
   }),
 });
+
+export const {
+  useSignUpUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation,
+} = appApi;
+
+export default appApi;
