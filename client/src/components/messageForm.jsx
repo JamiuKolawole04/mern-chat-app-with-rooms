dca56d25457277ee092025901daa8c996dfc2cfc
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 09:47:33 +0100

    chore: adding message view form

--- client/src/components/messageForm.jsx
+++ client/src/components/messageForm.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { Fragment } from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+
+import "./message-form.css";
 
 export const MessageForm = () => {
-  return <div>MessageForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Fragment>
+      <div className="messages-output"></div>
+
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col md={11}>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="Your message"
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col md={1}>
+            <Button
+              variant="primary"
+              type="submit"
+              style={{ width: "100%", backgroundColor: "orange" }}
+            >
+              <i className="fas fa-paper-plane"></i>
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Fragment>
+  );
 };
--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Fragment } from "react";
+import { ListGroup } from "react-bootstrap";
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const rooms = ["first room", "second room", "third room"];
+  return (
+    <Fragment>
+      <h2>Available rooms</h2>
+
+      <ListGroup>
+        {rooms.map((room, _i) => (
+          <ListGroup.Item key={_i}>{room}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      <h2>Members</h2>
+    </Fragment>
+  );
 };
