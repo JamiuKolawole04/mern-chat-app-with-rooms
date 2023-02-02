8a1670a5b0589873e5c2c7c5dba52da56a84f096
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 09:16:43 +0100

    chore: addig chat and sidenav

--- client/src/pages/chat.jsx
+++ client/src/pages/chat.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Sidebar, MessageForm } from "../components";
 
 export const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <Sidebar />
+        </Col>
+
+        <Col md={8}>
+          <MessageForm />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
