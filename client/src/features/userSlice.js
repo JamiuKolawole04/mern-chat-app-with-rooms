1e7a9199831e54aec40612c219662493ffeb2226
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 23:56:39 +0100

    feat: adding morgan and creating user slice

---
+++ client/src/features/userSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+import appApi from "../services/appApi";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {},
+});
