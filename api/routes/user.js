230e147e277c16fed1a5c0d378be2d0982b5c81c
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-02 22:52:13 +0100

    chore: adding error handling excemption for register route

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,9 +8,21 @@ router.post("/", async (req, res) => {
     const { name, email, password, picture } = req.body;
     const user = await User.create({ name, email, password, picture });
 
-    res.status(201).json(user);
+    res.status(201).json({
+      message: "user created successfully",
+      user,
+    });
   } catch (err) {
-    res.status(500).json(err);
+    let message;
+    if (err.code === 1100) {
+      message = "user already exists";
+    } else {
+      message = err.message;
+    }
+    res.status(500).json({
+      message,
+      err,
+    });
   }
 });
 
